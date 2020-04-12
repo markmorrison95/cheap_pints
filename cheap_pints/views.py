161e4a8649e3913f8570e8377aada9dae2f75916
Mark Morrison <markmorrison058@gmail.com>
2020-04-12 20:27:39 +0100

    h

--- cheap_pints/views.py
+++ cheap_pints/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from django.views.generic.base import TemplateView
 from pip._vendor import requests
-from django.contrib.gis.utils import GeoIP
+from django.contrib.gis.geoip import GeoIP
 
 # Create your views here.
 def index(request):
@@ -15,25 +15,13 @@ def barList(request):
     response = render(request, TEMPLATE, context={})
     return response
 
-def geoLoc(request):
+def geoLoc(request, **kwargs):
     key = 'AIzaSyBriJsnGZXUppVFg-q7cr2VpqHRmm7kczM'
     access_key = '397013fc76d5de24d0cc0b04b52e2aa6'
-    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
-    if x_forwarded_for:
-        ip_address = x_forwarded_for.split(',')[0]
-    else:
-        ip_address = request.META.get('REMOTE_ADDR')
-    
-    ip_address = '86.137.37.113'
-    g = GeoIP()
-    ip = request.META.get('REMOTE_ADDR', None)
-    if ip:
-        city = g.city(ip)['latitude']
-        city = g.city(ip)['longitude']
-    response = requests.get('http://api.ipstack.com/'+ip_address+'?access_key='+access_key+'&output=json')
-    geodata = response.json()
-    lat = str(geodata['latitude'])
-    lng = str(geodata['longitude'])
+    # response = requests.get('http://api.ipstack.com/'+ip_address+'?access_key='+access_key+'&output=json')
+    # geodata = response.json()
+    # lat = str(geodata['latitude'])
+    # lng = str(geodata['longitude'])
     print(lat + ',' + lng)
     response2 = requests.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+lat+','+lng+'&radius=1500&key='+key)
     nearby = response2.json()
