c26561f159cec534b9f15f1b24c946f6a7445b70
Mark Morrison <markmorrison058@gmail.com>
2020-06-03 17:48:14 +0100

    urls

--- cheap_pints/urls.py
+++ cheap_pints/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from cheap_pints import views
-from cheap_pints.views import AddBar
+from cheap_pints.views import AddBar, AddBeer
+from django.urls import include, re_path
+from cheap_pints import search
 app_name = 'cheap_pints'
 
 urlpatterns = [
@@ -8,5 +10,8 @@ urlpatterns = [
     path('bars/<str:value>/', views.barList, name='bars'),
     path('add_bar/', AddBar.as_view(), name='add_bar'),
     path('bar/<str:id>/', views.bar, name='bar' ),
-    path('search.json', views.autocompleteModel, name='autocomplete')
+    path('barsearch/', search.autocompleteBars, name='autocompleteBars'),
+    path('beernamesearch/', search.autocompleteBeerNames, name='autocompleteBeerName'),
+    path('beerbrandsearch/', search.autocompleteBeerBrands, name='autocompleteBeerBrands'),
+    path('add_beer/<slug:barname_slug>/<str:id>/', AddBeer.as_view(), name='add_beer'),
 ]
