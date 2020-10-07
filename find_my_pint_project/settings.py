0bceb9697b334b748948a94d7373df6b120e615c
Mark Morrison <markmorrison058@gmail.com>
2020-10-07 12:45:44 +0100

    update to requiremnets and DB

--- find_my_pint_project/settings.py
+++ find_my_pint_project/settings.py
@@ -114,22 +114,22 @@ WSGI_APPLICATION = 'find_my_pint_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'Cheap_Pints_Database',
-        'USER':'markmorrison_cheapPints',
-        'PASSWORD':'Sunshine5',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'Cheap_Pints_Database',
+#         'USER':'markmorrison_cheapPints',
+#         'PASSWORD':'Sunshine5',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
